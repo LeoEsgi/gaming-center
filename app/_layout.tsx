284d9d2ba201ee11a0d9e9ab1e64a83802001e62
Léo <Daviere-leo@hotmail.fr>
2024-06-01 01:42:13 +0200

    :art: Remove light theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DarkTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
